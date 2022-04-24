ac696936927827d6f5fa79bd14ef53f631e4b9d7
Matthew Carlson <carlson.matt.r@gmail.com>
2022-04-23 20:05:15 -0700

    HTML can now be created

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,22 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-getDocument =
+getDocument = require("./src/getSite.js");
+
+const init = () => {
+    inquirer
+    .prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the employee's name?"
+        },
+    ])
+    .then((answers => {  
+        const siteContent = getDocument(answers);
+        fs.writeFile(`./dist/Site.html`, siteContent, (err) =>
+        err ? console.log("Site Generation failed.") : console.log("Site Created!")
+        );
+    }))
+}
+
+init()
